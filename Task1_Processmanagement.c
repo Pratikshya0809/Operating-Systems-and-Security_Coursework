f4d91982a87f26c5cae5db881ba781f1922b4fd5
Pratikshya0809 <pratikshyakunwar0809@gmail.com>
2026-07-05 00:33:21 +0545

    Modify threads to demonstrate race condition on shared counter

--- Task1_Processmanagement.c
+++ Task1_Processmanagement.c
@@ -2,22 +2,31 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-void *worker(void *arg) {
-    int id = *(int *)arg;
-    printf("Thread %d is running\n", id);
+#define NUM_THREADS 4
+#define INCREMENTS_PER_THREAD 100000
+
+long shared_counter = 0;   /* shared, unprotected */
+
+void *increment_unsafe(void *arg) {
+    for (int i = 0; i < INCREMENTS_PER_THREAD; i++) {
+        shared_counter++;   /* NOT atomic -> race condition */
+    }
     return NULL;
 }
 
 int main() {
-    pthread_t threads[3];
-    int ids[3] = {1, 2, 3};
+    pthread_t threads[NUM_THREADS];
+    long expected = (long)NUM_THREADS * INCREMENTS_PER_THREAD;
 
-    for (int i = 0; i < 3; i++)
-        pthread_create(&threads[i], NULL, worker, &ids[i]);
+    for (int i = 0; i < NUM_THREADS; i++)
+        pthread_create(&threads[i], NULL, increment_unsafe, NULL);
 
-    for (int i = 0; i < 3; i++)
+    for (int i = 0; i < NUM_THREADS; i++)
         pthread_join(threads[i], NULL);
 
-    printf("All threads finished\n");
+    printf("Expected value       : %ld\n", expected);
+    printf("Unsynchronized result: %ld\n", shared_counter);
+    printf("(If these differ, that's the race condition)\n");
+
     return 0;
 }
